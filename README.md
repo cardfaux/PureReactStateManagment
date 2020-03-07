c2aa3f8eb5cdd16627ca4c214ed94e2858fa732d
James Hagood <cardfaux@tutanota.com>
2020-03-07 15:37:23 -0600

    added new files for useReducer Patterns

--- README.md
+++ README.md
@@ -7,3 +7,27 @@
 ## Counter.js is a simple example of state in a class based component
 
 ## Turns out SCSS files is Auto Compiled With Parcel just write The File and import it
+
+## What Not To Do With StateManagment
+
+#### Props have PropTypes State does not.
+
+#### if you can do it with props dont store it in state
+
+#### If youre not rendering it you dont need to store it in state
+
+#### Only stuff that when it changes i want to trigger a reRender
+
+#### Break Things Into Helper Methods
+
+#### useRef
+
+#### const countRef = React.useRef();
+
+#### useRef() gives us an object, { current: null }. It has the current property on it
+
+#### Primitives are passed by value non-Primitives are passed by reference, this way it keeps referencing tge same obkect..
+
+#### we can use countRef.current to compare new values and old values.
+
+#### useReducer Handles more complex state
--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import Counter from './components/Counter';
+import id from 'uuid/v4';
 
-const App = (props) => {
-  return <Counter />;
+import Grudges from './components/Grudges';
+import NewGrudge from './components/NewGrudge';
+
+import initialState from '../public/initialState';
+
+const App = () => {
+  const [grudges, setGrudges] = useState(initialState);
+
+  const addGrudge = (grudge) => {
+    grudge.id = id();
+    grudge.forgiven = false;
+    // Brandnew array containg newGrudge and copies of all the old ones
+    setGrudges([grudge, ...grudges]);
+  };
+
+  const toggleForgiveness = (id) => {
+    setGrudges(
+      grudges.map((grudge) => {
+        if (grudge.id !== id) return grudge;
+        return { ...grudge, forgiven: !grudge.forgiven };
+      })
+    );
+  };
+
+  return (
+    <div className='Application'>
+      <NewGrudge onSubmit={addGrudge} />
+      <Grudges grudges={grudges} onForgive={toggleForgiveness} />
+    </div>
+  );
 };
 
 export default App;
